6e2eb86280e00c2b0d5faf97847eb674b30d8e19
Don Harris <dharris@sudoers.com>
2026-09-16 10:46:25 -0400

    update script to pull in DROP list

--- update-blocked-ips.sh
+++ update-blocked-ips.sh
@@ -8,11 +8,17 @@ curl -sS https://www.spamhaus.org/drop/drop.txt > /tmp/spamhaus.txt
 echo "# Start Spamhaus List" >> /etc/nginx/conf.d/blocklist.conf
 grep -v ^\; /tmp/spamhaus.txt | awk '{ print "deny "$1";"}' >> /etc/nginx/conf.d/blocklist.conf
 
+# Add Spamhaus DROP (Don't Route or Peer list)
+curl -sS https://www.spamhaus.org/drop/drop.txt > /tmp/spamhaus_drop.txt
+echo "# Start Spamhaus DROP List" >> /etc/nginx/conf.d/blocklist.conf
+grep -v ^\; /tmp/spamhaus_drop.txt >> /etc/nginx/conf.d/blocklist.conf
+
 # Add Country Block lists
 # Russia
 echo "# Start Russia blocklist" >> /etc/nginx/conf.d/blocklist.conf
 curl -sS https://raw.githubusercontent.com/ipverse/country-ip-blocks/master/country/ru/ipv4-aggregated.txt > /tmp/ru.txt
 grep -v ^\# /tmp/ru.txt | awk '{ print "deny "$1";"}' >> /etc/nginx/conf.d/blocklist.conf
+
 # Singapore
 echo "# Start Signapore blocklist" >> /etc/nginx/conf.d/blocklist.conf
 curl -sS https://raw.githubusercontent.com/ipverse/country-ip-blocks/master/country/sg/ipv4-aggregated.txt > /tmp/sg.txt
